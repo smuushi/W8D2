a5075201926fca3832a9aeee86e63bcd0628fe4f
Michael Gastello <michaelgastello@aol.com>
2023-02-28 16:48:11 -0500

    adderererer

--- skeleton/phase_2_enumerable.js
+++ skeleton/phase_2_enumerable.js
@@ -1,12 +1,39 @@
-Array.prototype.myEach = function(callback) {
+Array.prototype.myEach = function(someFunction) {
 
     for (let i = 0 ; i < this.length ; i++) {
         const theElement = this[i];
-        callback(theElement);
-    }
+        someFunction(theElement);
+    };
 
-}
+};
 
 const hey = function() {
-    console.log("hello ")
+    console.log("hello ");
+};
+//////////////////////////////////////
+Array.prototype.myMap = function(callbackk) {
+    let mappedArray = [];
+    // this.myEach((el) => mappedArray.push(callbackk(el)));
+    this.myEach( function(el) {
+        mappedArray.push(callbackk(el));
+    });
+    return mappedArray;
+};
+
+const adder = function(el) {
+    let added = el;
+    added = added + 1;
+    return added;
 }
+
+const adderer = function(el) {
+    return el + 1;
+}
+
+function addererer(el) {
+    return el + 1;
+}
+
+const addererer = (el) => {
+    return el + 1;
+};
