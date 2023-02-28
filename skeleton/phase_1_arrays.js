da7820e8c7b1277a78350f53d15a4e43b7ae3802
Michael Gastello <michaelgastello@aol.com>
2023-02-28 15:23:22 -0500

    done with phase 1

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -6,7 +6,7 @@ Array.prototype.uniq = function() {
         };
     });
     return uniqArray;
-}
+};
 
 Array.prototype.twoSum = function() {
     let pairs = [];
@@ -14,9 +14,20 @@ Array.prototype.twoSum = function() {
         for (let j = i + 1 ; j < this.length ; j++) {
             if (this[i] + this[j] === 0) {
                 pairs.push([i, j]); 
-            }
-        }
-    }
+            };
+        };
+    };
     return pairs;
 
-}
+};
+
+Array.prototype.transpose = function() {
+    const amountOfNewArrays = this[0].length;
+    let transposedOuterArray = [];
+    for (let i = 0; i < amountOfNewArrays; i++) {
+        let transposedInnerArray = [];
+        this.forEach((originalSubArray) => transposedInnerArray.push(originalSubArray[i]));
+        transposedOuterArray.push(transposedInnerArray);
+    };
+    return transposedOuterArray;
+};
