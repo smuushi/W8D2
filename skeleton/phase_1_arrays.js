efc848e295f227259182860004bf93cf6c38f029
smuushi <mishih780@gmail.com>
2023-02-28 15:05:40 -0500

    doing well

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -7,3 +7,16 @@ Array.prototype.uniq = function() {
     });
     return uniqArray;
 }
+
+Array.prototype.twoSum = function() {
+    let pairs = [];
+    for (let i = 0; i < this.length ; i++) {
+        for (let j = i + 1 ; j < this.length ; j++) {
+            if (this[i] + this[j] === 0) {
+                pairs.push([i, j]); 
+            }
+        }
+    }
+    return pairs;
+
+}
