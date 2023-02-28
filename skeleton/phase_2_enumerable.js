0188a14bd1fe7f44ff1589ac3545c3a58f74bb33
smuushi <mishih780@gmail.com>
2023-02-28 17:33:07 -0500

    asdf

--- skeleton/phase_2_enumerable.js
+++ skeleton/phase_2_enumerable.js
@@ -24,16 +24,52 @@ const adder = function(el) {
     let added = el;
     added = added + 1;
     return added;
-}
+};
 
 const adderer = function(el) {
     return el + 1;
-}
+};
 
 function addererer(el) {
     return el + 1;
-}
+};
 
-const addererer = (el) => {
+const adderererer = (el) => {
     return el + 1;
 };
+
+// node
+// > inject
+// > function(acc, el) {
+//     does something with acc and el... ;
+// };
+
+Array.prototype.myReduce = function(inject, initialValue) {
+    if (initialValue === undefined) {
+        initialValue = this[0];
+        this.shift();
+    };
+
+    let accumulator = initialValue;
+    
+    // sum = this.myEach((el) => inject(sum, el));
+
+    this.myEach( function(el) {
+        debugger
+        accumulator = inject(accumulator, el);
+
+    });
+
+
+    return accumulator;
+};
+
+
+// [1, 2, 3].myReduce(
+//     (function(acc, el) { return acc + el })
+// ); // => 6
+
+// [1, 2, 3].myReduce(
+//     (function(acc, el) { return acc + el }), 
+//     (25)
+// ); // => 31
